9dce0db571d5170e7247190cf165e9d58baa3725
Salvador Martinez de Bartolome Izquierdo <salvador@scripps.edu>
2020-05-08 18:27:56 -0700

    adding a version with GUI and with command line

--- src/main/java/edu/scripps/yates/censustmt2msstatstmt/CensusTMT2MSstatsTMT.java
+++ src/main/java/edu/scripps/yates/censustmt2msstatstmt/CensusTMT2MSstatsTMT.java
@@ -4,7 +4,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -13,7 +13,6 @@ import java.util.stream.Collectors;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
-import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 import org.apache.commons.io.FilenameUtils;
@@ -27,146 +26,179 @@ import edu.scripps.yates.census.read.model.interfaces.QuantifiedProteinInterface
 import edu.scripps.yates.census.read.util.QuantificationLabel;
 import edu.scripps.yates.utilities.appversion.AppVersion;
 import edu.scripps.yates.utilities.maths.Maths;
+import edu.scripps.yates.utilities.progresscounter.ProgressCounter;
+import edu.scripps.yates.utilities.progresscounter.ProgressPrintingType;
 import edu.scripps.yates.utilities.properties.PropertiesUtil;
 import edu.scripps.yates.utilities.proteomicsmodel.Amount;
 import edu.scripps.yates.utilities.proteomicsmodel.enums.AmountType;
+import edu.scripps.yates.utilities.swing.CommandLineProgramGuiEnclosable;
 import gnu.trove.list.TDoubleList;
 import gnu.trove.list.array.TDoubleArrayList;
 import gnu.trove.map.TObjectDoubleMap;
 import gnu.trove.map.custom_hash.TObjectDoubleCustomHashMap;
 import gnu.trove.map.hash.THashMap;
 import gnu.trove.map.hash.TObjectDoubleHashMap;
+import gnu.trove.set.hash.THashSet;
 
-public class CensusTMT2MSstatsTMT {
+public class CensusTMT2MSstatsTMT extends CommandLineProgramGuiEnclosable {
 	private final static Logger log = Logger.getLogger(CensusTMT2MSstatsTMT.class);
 	private static AppVersion version;
-	private static Options options;
-	private final File inputFile;
-	private final boolean uniquePeptides;
-	private final boolean useRawIntensity;
-	private final PSMSelectionType psmSelection;
-	private final File experimentalDesignFile;
-	private final String experimentalDesignSeparator;
+	private File inputFile;
+	private boolean uniquePeptides;
+	private boolean useRawIntensity;
+	private PSMSelectionType psmSelection;
+	private File experimentalDesignFile;
+	private String experimentalDesignSeparator;
+	private String decoyPrefix;
 	private ExperimentalDesign ed;
+	private int minNumPeptides;
 
 	public CensusTMT2MSstatsTMT(File inputFile, File experimentalDesignFile, String experimentalDesignSeparator,
-			boolean uniquePeptides, boolean useRawIntensity, PSMSelectionType psmSelection) {
+			boolean uniquePeptides, boolean useRawIntensity, String decoyPrefix, PSMSelectionType psmSelection) {
+		super(null);// we dont use that functionality to create GUI here
 		this.inputFile = inputFile;
 		this.experimentalDesignFile = experimentalDesignFile;
 		this.experimentalDesignSeparator = experimentalDesignSeparator;
 		this.uniquePeptides = uniquePeptides;
 		this.useRawIntensity = useRawIntensity;
+		this.decoyPrefix = decoyPrefix;
 		this.psmSelection = psmSelection;
 	}
 
+	public CensusTMT2MSstatsTMT(Options options) {
+		super(options);
+
+	}
+
 	public static void main(String[] args) {
-		setupCommandLineOptions();
-		File inputFile = null;
-		File experimentalDesignFile = null;
-		final String experimentalDesignSeparator = ",";
-		boolean uniquePeptides = false;
-		boolean useRawIntensity = false;
-		PSMSelectionType psmSelection = PSMSelectionType.HIGHEST;
+
 		final CommandLineParser parser = new DefaultParser();
+		final Options options = setupCommandLineOptions();
+		CensusTMT2MSstatsTMT c = null;
 		try {
+			c = new CensusTMT2MSstatsTMT(options);
+
 			final CommandLine cmd = parser.parse(options, args);
-			if (cmd.hasOption("i")) {
-				final String iOptionValue = cmd.getOptionValue("i");
-				inputFile = new File(iOptionValue);
-				final File parentFile = inputFile.getParentFile();
-				if (parentFile == null || !inputFile.exists()) {
-					inputFile = new File(System.getProperty("user.dir") + File.separator + iOptionValue);
-				}
-
-				if (!inputFile.exists()) {
-					errorInParameters("Input file '-i " + iOptionValue + "' doesn't exist or is not found");
-				}
-			} else {
-				errorInParameters("Input file is missing");
-			}
-			if (cmd.hasOption("ed")) {
-				final String edOptionValue = cmd.getOptionValue("ed");
-				experimentalDesignFile = new File(edOptionValue);
-				final File parentFile = experimentalDesignFile.getParentFile();
-				if (parentFile == null || !experimentalDesignFile.exists()) {
-					experimentalDesignFile = new File(System.getProperty("user.dir") + File.separator + edOptionValue);
-				}
-
-				if (!experimentalDesignFile.exists()) {
-					errorInParameters(
-							"Experimental design file '-ed " + edOptionValue + "' doesn't exist or is not found");
-				}
-			} else {
-				errorInParameters("Experimental design file is missing");
-			}
-			if (cmd.hasOption("ps")) {
-				try {
-					psmSelection = PSMSelectionType.valueOf(cmd.getOptionValue("ps"));
-				} catch (final Exception e) {
-					e.printStackTrace();
-					errorInParameters("option 'ps' ('" + options.getOption("ps").getArgName()
-							+ "') is not valid. Valid values are " + PSMSelectionType.printValidValues());
-				}
-
-			}
-			log.info("Using ps option " + psmSelection);
-
-			if (cmd.hasOption("u")) {
-				uniquePeptides = true;
-				log.info("Using only unique peptides.");
-			} else {
-				log.info("Using all peptides regardless of their uniqueness.");
-			}
-
-			if (cmd.hasOption("r")) {
-				useRawIntensity = true;
-				log.info("Using raw intensities.");
-			} else {
-				log.info("Using normalized intensities.");
-			}
-
-			final CensusTMT2MSstatsTMT c = new CensusTMT2MSstatsTMT(inputFile, experimentalDesignFile,
-					experimentalDesignSeparator, uniquePeptides, useRawIntensity, psmSelection);
+			c.setCommandLine(cmd);
 			c.run();
 		} catch (final ParseException e) {
-			errorInParameters(e.getMessage());
+			e.printStackTrace();
+			if (c != null) {
+				c.errorInParameters(e.getMessage());
+			}
 		} catch (final Exception e) {
 			e.printStackTrace();
 			System.err.println(e.getMessage());
 		}
 	}
 
-	private void run() throws IOException {
-		final FileWriter fw = new FileWriter(getOutputFile());
+	private void runConversion() throws IOException {
+		System.out.println("Starting conversion of file " + inputFile.getAbsolutePath());
+		final File outputFile = getOutputFile();
+		final FileWriter fw = new FileWriter(outputFile);
+		System.out.println("Output file: " + outputFile.getAbsolutePath());
 		// header
 		printHeader(fw);
 		final Map<QuantificationLabel, QuantCondition> conditionsByLabels = getExperimentalDesign()
 				.getConditionByLabel();
+		System.out.println("Reading input file...");
 		final CensusOutParser parser = new CensusOutParser(this.inputFile, conditionsByLabels);
-		final Collection<QuantifiedPSMInterface> psms = parser.getPSMMap().values();
-		log.info(psms.size() + " psms read");
+		final List<QuantifiedPSMInterface> psms = new ArrayList<QuantifiedPSMInterface>();
+		psms.addAll(parser.getPSMMap().values());
+		System.out.println(psms.size() + " PSMs read from input file.");
+
+		// filter by min number of peptides per protein
+
+		final ProgressCounter counter = new ProgressCounter(psms.size(), ProgressPrintingType.PERCENTAGE_STEPS, 0);
+		Iterator<QuantifiedPSMInterface> psmsIterator = psms.iterator();
+		if (decoyPrefix != null && !"".equals(decoyPrefix)) {
+			final Set<QuantifiedProteinInterface> discardedProteins = new THashSet<QuantifiedProteinInterface>();
+			final int initialPSMs = psms.size();
+			while (psmsIterator.hasNext()) {
+				final QuantifiedPSMInterface psm = psmsIterator.next();
+
+				for (final QuantifiedProteinInterface protein : psm.getQuantifiedProteins()) {
+					// //decoy
+					if (decoyPrefix != null && protein.getAccession().startsWith(decoyPrefix)) {
+						psmsIterator.remove();
+						discardedProteins.add(protein);
+						break;
+					}
+				}
+			}
+			System.out.print("\n-" + discardedProteins.size() + " proteins discarded as DECOYs.");
+			System.out.println(
+					" Now working with " + psms.size() + " PSMs (" + (initialPSMs - psms.size()) + " discarded).\n");
+		}
+		if (minNumPeptides > 1 || uniquePeptides) {
+			int psmsDiscardedByUniqueness = 0;
+			final Set<QuantifiedProteinInterface> discardedProteins = new THashSet<QuantifiedProteinInterface>();
+			final int initialPSMs = psms.size();
+			// filtering by minNumPeptides
+			psmsIterator = psms.iterator();
+			while (psmsIterator.hasNext()) {
+				counter.increment();
+				final String printIfNecessary = counter.printIfNecessary();
+				if (!"".equals(printIfNecessary)) {
+					System.out.println("Filtering data..." + printIfNecessary);
+				}
+				final QuantifiedPSMInterface psm = psmsIterator.next();
+				if (uniquePeptides && psm.getQuantifiedProteins().size() > 1) {
+					psmsIterator.remove();
+					psmsDiscardedByUniqueness++;
+					continue;
+				}
+				boolean anyProteinIsValid = false;
+				for (final QuantifiedProteinInterface protein : psm.getQuantifiedProteins()) {
+					if (protein.getAccession().equals("A0A096MJY1")) {
+						log.info("asdf");
+					}
+					final Set<String> peptideCharges = new THashSet<String>();
+					protein.getQuantifiedPSMs().stream()
+							.forEach(p -> peptideCharges.add(p.getFullSequence() + p.getChargeState()));
+					if (peptideCharges.size() >= minNumPeptides) {
+						anyProteinIsValid = true;
+					} else {
+						discardedProteins.add(protein);
+					}
+				}
+				if (!anyProteinIsValid) {
+					psmsIterator.remove();
+				}
+			}
+			System.out.print("\n- " + discardedProteins.size() + " proteins discarded for not having at least "
+					+ minNumPeptides + " peptides (sequence+charge).");
+			if (uniquePeptides) {
+				System.out.print(" " + psmsDiscardedByUniqueness + " PSMs discarded because they are not unique.");
+			}
+			System.out.println(
+					" Now working with " + psms.size() + " PSMs (" + (initialPSMs - psms.size()) + " discarded).");
+
+		}
+
 		// create a map by sequence
-		final Map<String, List<QuantifiedPSMInterface>> psmsBySequence = new THashMap<String, List<QuantifiedPSMInterface>>();
+		final Map<String, List<QuantifiedPSMInterface>> psmsBySequenceAndCharge = new THashMap<String, List<QuantifiedPSMInterface>>();
 		for (final QuantifiedPSMInterface psm : psms) {
 			final String seq = psm.getBeforeSeq() + "." + psm.getSequence() + "." + psm.getAfterSeq() + "_"
 					+ psm.getChargeState();
-			if (!psmsBySequence.containsKey(seq)) {
-				psmsBySequence.put(seq, new ArrayList<QuantifiedPSMInterface>());
+			if (!psmsBySequenceAndCharge.containsKey(seq)) {
+				psmsBySequenceAndCharge.put(seq, new ArrayList<QuantifiedPSMInterface>());
 			}
-			psmsBySequence.get(seq).add(psm);
+			psmsBySequenceAndCharge.get(seq).add(psm);
 		}
+
 		// iterate over it. Sort first the sequences so that they will appear in order
 		// in the output
-		final Set<String> seqsSet = psmsBySequence.keySet();
+		final Set<String> seqsSet = psmsBySequenceAndCharge.keySet();
 		final List<String> seqList = seqsSet.stream().sorted().collect(Collectors.toCollection(ArrayList::new));
+		final Set<String> validProteins = new THashSet<String>();
 		for (final String seq : seqList) {
-			final List<QuantifiedPSMInterface> psmsOfSeq = psmsBySequence.get(seq);
+			final List<QuantifiedPSMInterface> psmsOfSeq = psmsBySequenceAndCharge.get(seq);
 
 			final List<String> accs = getAccToPrint(psmsOfSeq);
-			if (accs.size() > 1 && this.uniquePeptides) {
-				log.debug("Skipping PSMs from peptide " + seq + " because not uniqueness");
-				continue;
-			}
+			final String acc = accs.get(0);
+			validProteins.add(acc);
+
 			final int charge = getChargeToPrint(psmsOfSeq);
 			final TObjectDoubleMap<QuantificationLabel> intensities = getIntensitiesToPrint(psmsOfSeq,
 					this.psmSelection, this.useRawIntensity);
@@ -177,12 +209,16 @@ public class CensusTMT2MSstatsTMT {
 			final String run = firstPSM.getMSRun().getRunId();
 			for (final QuantificationLabel label : intensities.keySet().stream().sorted()
 					.collect(Collectors.toList())) {
-				printOutputLine(fw, label, intensities.get(label), accs.get(0), charge, peptideSequence, psm, run);
+				printOutputLine(fw, label, intensities.get(label), acc, charge, peptideSequence, psm, run);
 			}
 
 		}
+		System.out.print("\n*******\nValid data: " + psms.size() + " PSMs");
+		System.out.print(", " + psmsBySequenceAndCharge.size() + " peptides (sequences+charge)");
+		System.out.println(", " + validProteins.size() + " proteins\n*******");
 		fw.close();
-		log.info("File written at " + getOutputFile().getAbsolutePath());
+		log.info("File written at " + outputFile.getAbsolutePath());
+		System.out.println("File ready at " + outputFile.getAbsolutePath());
 	}
 
 	private void printHeader(FileWriter fw) throws IOException {
@@ -265,9 +301,14 @@ public class CensusTMT2MSstatsTMT {
 					final String conditionName = split[0];
 					final int channel = Integer.valueOf(split[1]);
 					final QuantificationLabel label = ExperimentalDesign.getTMT6LabelFromChannel(channel);
+					// for the average, don't use the zero
+					if (Double.compare(0.0, amount.getValue()) == 0) {
+						continue;
+					}
 					if (!toAverage.containsKey(label)) {
 						toAverage.put(label, new TDoubleArrayList());
 					}
+
 					toAverage.get(label).add(amount.getValue());
 				}
 			}
@@ -385,33 +426,131 @@ public class CensusTMT2MSstatsTMT {
 
 	}
 
-	private static void setupCommandLineOptions() {
+	protected static Options setupCommandLineOptions() {
 		// create Options object
-		options = new Options();
+		final Options options = new Options();
 
 		// add t option
 		options.addRequiredOption("i", "input", true, "Path to the input file.");
-		options.addRequiredOption("ed", "experimental_design", true, "Path to the experimental design file.");
+		options.addRequiredOption("an", "annotation", true, "Path to the experimental design file.");
 		options.addOption("ps", "psm_selection", true,
 				"[OPTIONAL] What to do with multiple PSMs of the same peptide (SUM, AVERAGE or HIGHEST). If not provided, HIGHEST will be choosen.");
 		options.addOption("u", "unique", false,
 				"[OPTIONAL] Use only unique peptides. If not provided, all peptides will be used.");
 		options.addOption("r", "raw", false,
 				"[OPTIONAL] Use of raw intensity. If not provided, normalized intensity will be used.");
-
+		options.addOption("d", "decoy", true,
+				"[OPTIONAL] Remove decoy hits. Decoys hits will have this prefix in their accession number. If not provided, no decoy filtering will be used.");
+		options.addOption("m", "minPeptides", true,
+				"[OPTIONAL] Minimum number of peptides+charge per protein. If not provided, even proteins with 1 peptide will be quantified");
+		return options;
 	}
 
-	private static void errorInParameters(String header) {
-		// automatically generate the help statement
-		final HelpFormatter formatter = new HelpFormatter();
-		if (header == null) {
-			formatter.printHelp("CensusTMT2MSstatsTMT -i [input file] -ed [experimental design file]", options);
+	@Override
+	public Options getCommandLineOptions() {
+
+		return setupCommandLineOptions();
+	}
+
+	@Override
+	public void run() throws Exception {
+		runConversion();
+	}
+
+	@Override
+	public String getTitleForFrame() {
+		return "CensusTMT2MSstatsTMT converter";
+	}
+
+	@Override
+	protected void setCommandLine(CommandLine cmd) {
+		File inputFile = null;
+		File experimentalDesignFile = null;
+		final String experimentalDesignSeparator = ",";
+		boolean uniquePeptides = false;
+		boolean useRawIntensity = false;
+		String decoyPrefix = null;
+		PSMSelectionType psmSelection = PSMSelectionType.HIGHEST;
+
+		if (cmd.hasOption("i")) {
+			final String iOptionValue = cmd.getOptionValue("i");
+			inputFile = new File(iOptionValue);
+			final File parentFile = inputFile.getParentFile();
+			if (parentFile == null || !inputFile.exists()) {
+				inputFile = new File(System.getProperty("user.dir") + File.separator + iOptionValue);
+			}
+
+			if (!inputFile.exists()) {
+				errorInParameters("Input file '-i " + iOptionValue + "' doesn't exist or is not found");
+			}
 		} else {
-			formatter.printHelp("CensusTMT2MSstatsTMT -i [input file] -ed [experimental design file]",
-					"\n\n************\n" + header + "\n************\n\n", options,
-					"Contact Salvador Martinez-Bartolome at salvador@scripps.edu for more help");
+			errorInParameters("Input file is missing");
 		}
-		System.exit(-1);
+		if (cmd.hasOption("an")) {
+			final String anOptionValue = cmd.getOptionValue("an");
+			experimentalDesignFile = new File(anOptionValue);
+			final File parentFile = experimentalDesignFile.getParentFile();
+			if (parentFile == null || !experimentalDesignFile.exists()) {
+				experimentalDesignFile = new File(System.getProperty("user.dir") + File.separator + anOptionValue);
+			}
+
+			if (!experimentalDesignFile.exists()) {
+				errorInParameters("Annotation file (experimental design) '-an " + anOptionValue
+						+ "' doesn't exist or is not found");
+			}
+		} else {
+			errorInParameters("Experimental design file is missing");
+		}
+		if (cmd.hasOption("ps")) {
+			try {
+				psmSelection = PSMSelectionType.valueOf(cmd.getOptionValue("ps"));
+			} catch (final Exception e) {
+				e.printStackTrace();
+				errorInParameters("option 'ps' ('" + cmd.getOptionValue("ps") + "') is not valid. Valid values are "
+						+ PSMSelectionType.printValidValues());
+			}
+
+		}
+		log.info("Using ps option " + psmSelection);
+
+		if (cmd.hasOption("u")) {
+			uniquePeptides = true;
+			log.info("Using only unique peptides.");
+		} else {
+			log.info("Using all peptides regardless of their uniqueness.");
+		}
+
+		if (cmd.hasOption("r")) {
+			useRawIntensity = true;
+			log.info("Using raw intensities.");
+		} else {
+			log.info("Using normalized intensities.");
+		}
+
+		if (cmd.hasOption("d")) {
+			decoyPrefix = cmd.getOptionValue("d");
+			if ("".equals(decoyPrefix)) {
+				decoyPrefix = null;
+			}
+		}
+		int minNumPeptides = 0;
+		if (cmd.hasOption("m")) {
+			minNumPeptides = Integer.valueOf(cmd.getOptionValue("m"));
+		}
+		log.info("Using d option for decoy prefix='" + decoyPrefix + "'");
+		this.inputFile = inputFile;
+		this.experimentalDesignFile = experimentalDesignFile;
+		this.experimentalDesignSeparator = experimentalDesignSeparator;
+		this.uniquePeptides = uniquePeptides;
+		this.useRawIntensity = useRawIntensity;
+		this.decoyPrefix = decoyPrefix;
+		this.psmSelection = psmSelection;
+		this.minNumPeptides = minNumPeptides;
+	}
+
+	@Override
+	public String printCommandLineSintax() {
+		return "CensusTMT2MSstatsTMT -i [input file] -an [annotation file]";
 	}
 
 }
